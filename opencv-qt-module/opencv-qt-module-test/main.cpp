86386e40e5e97d5c707437554698865f29fca4b7
Vasiliy Alchakov <v.alchakov@gmail.com>
2022-06-05 15:59:47 +0300

    OpenCV imread function test added

--- opencv-qt-module/opencv-qt-module-test/main.cpp
+++ opencv-qt-module/opencv-qt-module-test/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using namespace std;
 
 int main()
 {
-    cout << "opencv-qt-module-test" << endl;
+    cout << "Start opencv-qt-module-test" << endl;
+
+    cv::Mat image = cv::imread("..\\..\\opencv-qt-module\\opencv-qt-module-test\\images\\OpenCV.png");
+
+    if (image.data)
+    {
+        cv::namedWindow("OpenCV");
+        cv::imshow("OpenCV", image);
+    }
+
+    cv::waitKey();
+    cv::destroyAllWindows();
+
+    cout << "Press any key to exit..." << endl;
+    cin.get();
+
     return 0;
 }
