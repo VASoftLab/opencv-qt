0ee2321da38e3f62f97c668ae37b349725286695
Vasiliy Alchakov <v.alchakov@gmail.com>
2022-06-06 15:48:46 +0300

    Capturing two webcams

--- opencv-qt-module/opencv-qt-module-test/main.cpp
+++ opencv-qt-module/opencv-qt-module-test/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace std;
 
@@ -8,6 +9,9 @@ int main()
 {
     cout << "Start opencv-qt-module-test" << endl;
 
+    //==========================================================================
+    // OpenCV Image Read
+    //==========================================================================
     cv::Mat image = cv::imread("..\\..\\opencv-qt-module\\opencv-qt-module-test\\images\\OpenCV.png");
 
     if (image.data)
@@ -18,6 +22,93 @@ int main()
 
     cv::waitKey();
     cv::destroyAllWindows();
+
+    cout << "Press any key to continue" << endl;
+    cin.get();
+
+    //==========================================================================
+    // OpenCV WebCam Read
+    //==========================================================================
+
+    cv::Mat frame;
+    cv::VideoCapture capture;
+
+    int deviceID = 0; // Default Camera
+    int apiID = cv::CAP_ANY;
+    capture.open(deviceID, apiID);
+
+    if (!capture.isOpened()) {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
+
+    //==========================================================================
+    // GRAB AND WRITE LOOP
+    //==========================================================================
+    cout << "Start grabbing" << endl << "Press any key to terminate" << endl;
+    for (;;)
+    {
+        // Wait for a new frame from camera and store it into 'frame'
+        capture.read(frame);
+        // Check if we succeeded
+        if (frame.empty()) {
+            cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+        // Show live and wait for a key with timeout long enough to show images
+        cv::imshow("LiveCamera", frame);
+        if (cv::waitKey(5) >= 0)
+            break;
+    }
+    //==========================================================================
+    cv::destroyAllWindows();
+
+    cout << "Press any key to continue" << endl;
+    cin.get();
+
+    //==========================================================================
+    // OpenCV WebCam Read
+    //==========================================================================
+
+    cv::Mat3b frameLeft;
+    cv::VideoCapture captureLeft;
+    cv::Mat3b frameRight;
+    cv::VideoCapture captureRight;
+
+    cv::Mat3b frameCombined;
+
+    int deviceIDLeft = 0; // Left Camera
+    int deviceIDRight = 1; // Right Camera
+
+    captureLeft.open(deviceIDLeft, cv::CAP_ANY);
+    captureRight.open(deviceIDRight, cv::CAP_ANY);
+
+    if (!captureLeft.isOpened())
+    {
+        cerr << "ERROR! Left Camera not ready!" << endl;
+        return 1;
+        cin.get();
+    }
+
+    if (!captureRight.isOpened())
+    {
+        cerr << "ERROR! Right Camera not ready!" << endl;
+        return 1;
+        cin.get();
+    }
+
+    for (;;)
+    {
+        captureLeft >> frameLeft;
+        captureRight >> frameRight;
+        cv::hconcat(frameLeft, frameRight, frameCombined);
+
+        cv::imshow("WebCamCombined", frameCombined);
+        if (cv::waitKey(5) >= 0)
+            break;
+    }
+    cv::destroyAllWindows();
+    //==========================================================================
 
     cout << "Press any key to exit..." << endl;
     cin.get();
--- opencv-qt-module/opencv-qt-module-test/opencv-qt-module-test.pro
+++ opencv-qt-module/opencv-qt-module-test/opencv-qt-module-test.pro
@@ -5,3 +5,14 @@ CONFIG -= qt
 
 SOURCES += \
         main.cpp
+
+INCLUDEPATH += D:\OpenCV\build\include
+
+LIBS += D:\opencv-build\bin\libopencv_core455.dll
+LIBS += D:\opencv-build\bin\libopencv_highgui455.dll
+LIBS += D:\opencv-build\bin\libopencv_imgcodecs455.dll
+LIBS += D:\opencv-build\bin\libopencv_imgproc455.dll
+LIBS += D:\opencv-build\bin\libopencv_features2d455.dll
+LIBS += D:\opencv-build\bin\libopencv_calib3d455.dll
+LIBS += D:\opencv-build\bin\libopencv_videoio455.dll
+
